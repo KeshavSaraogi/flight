483ef03257ccdcc52004efa1739f1eeb55f71c51
Keshav Saraogi <tuj49866@temple.edu>
2024-08-18 16:38:36 +0530

    creating a database

---
+++ databricks/connection/create_database.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE DATABASE IF NOT EXISTS cleansed_data;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE DATABASE IF NOT EXISTS mart_data;
